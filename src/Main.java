b356bd2b7ccc1dc78794c2c081153703500ba94b
cedricm <cedric.molinari0@gmail.com>
2024-03-06 09:41:38 +0100

    Exo 9 - fonction somme d'un tableau donné

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.OptionalInt;
 import java.util.Scanner;
@@ -6,29 +7,22 @@ import java.util.stream.*;
 public class Main {
     public static void main(String[] args) {
 
-        /* Exercice 2 : Écrire une fonction qui retourne le plus grand nombre parmi 2 nombres données. */
-        Scanner scanner = new Scanner(System.in);
-        try {
-            System.out.println("Saisir un nombre : ");
-            int nombreSaisi1 = scanner.nextInt();
-            System.out.println("Saisir un 2e nombre : ");
-            int nombreSaisi2 = scanner.nextInt();
-            System.out.println("Le nombre le plus grand est : " + nombrePlusGrand(nombreSaisi1, nombreSaisi2));
-        } catch (InputMismatchException e) {
-            System.out.println("Merci de saisir des nombres valides.");
-        }
+        /* Exercice 3 : Écrire une fonction qui retourne la somme des nombres d’un tableau donné en paramètre */
+
+        int parametreTableau = sommeTableau(4, 98, 65, 34, 3);
+
+        System.out.println("La somme du tableau donné en paramètre est : " + parametreTableau);
     }
-    public static int nombrePlusGrand(int a, int b) {
-        IntStream nombres = IntStream.of(a, b);
-        return nombres.max().getAsInt();
+    public static int sommeTableau(int... nombres) {
+        return IntStream.of(nombres)
+                .sum();
     }
 
         /*Java fonctions
 
-        Exercice 3
-        Écrire une fonction qui retourne la somme des nombres d’un tableau donné en paramètre
         Exercice 4
-        Écrire un programme qui propose à l'utilisateur d’utiliser les fonctions que vous avez créées dans le terminal. En fonction de sa réponse, vous lui demandez les informations nécessaires puis vous retournez la réponse.
+        Écrire un programme qui propose à l'utilisateur d’utiliser les fonctions que vous avez créées dans le terminal.
+        En fonction de sa réponse, vous lui demandez les informations nécessaires puis vous retournez la réponse.
         TD4 - Java - Objets et héritage
         Exercice 1
         Créez une classe “Personne” qui possède des attributs pour le nom, le prénom et l’âge.
